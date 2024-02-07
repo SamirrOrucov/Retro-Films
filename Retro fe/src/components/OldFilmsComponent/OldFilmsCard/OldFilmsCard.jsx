f32ec8b8dd29d30df42fea5397ad5de6cc9fd836
Samir Orucov <samiroruclu10@gmail.com>
2024-02-08 00:34:32 +0400

    old film section added,pagination added

--- Retro fe/src/components/OldFilmsComponent/OldFilmsCard/OldFilmsCard.jsx
+++ Retro fe/src/components/OldFilmsComponent/OldFilmsCard/OldFilmsCard.jsx
@@ -1,35 +1,41 @@
-import React from 'react'
-
-function OldFilmsCard( {year,category,image,title,desc,duration}) {
+import React from "react";
+import "./OldFilmsCards.scss";
+function OldFilmsCard({ year, category, image, title, desc, duration }) {
+  const { hours, minutes } = timeConvert(duration);
+  function timeConvert(duration) {
+    const minutes = duration % 60;
+    const hours = (duration - minutes) / 60;
+    return { hours, minutes };
+  }
   return (
-    <div className='oldFilmsCard'>
-
-        <div className="oldFilmsCard_container">
-            <div className="top">
-                <div className="year">
-                    <p>{year}</p>
-                </div>
-                <div className="catgory">
-                    <button>{category}</button>
-                </div>
-            </div>
-            <div className="image">
-                <img src={image} alt="" />
-            </div>
-            <div className="middle">
-                <div className="title">
-                    <p>{title}</p>
-                </div>
-                <div className="desc">
-                    {desc}
-                </div>
-            </div>
-            <div className="duration">
-                <p><span>Duration:</span>{duration}</p>
-            </div>
+    <div className="oldFilmsCard">
+      <div className="oldFilmsCard_container">
+        <div className="top">
+          <div className="year">
+            <p>{year}</p>
+          </div>
+          <div className="catgory">
+            <button>{category}</button>
+          </div>
         </div>
+        <div className="image">
+          <img src={image} alt="" />
+        </div>
+        <div className="middle">
+          <div className="title">
+            <p>{title}</p>
+          </div>
+          <div className="desc">{desc}</div>
+        </div>
+        <div className="duration">
+          <p>
+            <span>Duration: </span>
+            {hours+ "h" + " " + minutes+"min"}
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default OldFilmsCard
+export default OldFilmsCard;
--- Retro fe/src/components/OldFilmsComponent/OldFilmsComponents.jsx
+++ Retro fe/src/components/OldFilmsComponent/OldFilmsComponents.jsx
@@ -1,46 +1,75 @@
-import React, { useEffect, useState } from 'react'
-import "./OldFilmsComponents.scss"
-import OldFilmsCard from './OldFilmsCard/OldFilmsCard'
-function OldFilmsComponents() {
-    const [dbData, setDbData] = useState([]);
-    async function fetchData() {
-      const response = await fetch("http://localhost:3003/film");
-      const data = await response.json();
-      setDbData(data);
-    }
-    useEffect(() => {
-      fetchData();
-    }, []);
-  
-  return (
-    <div className='oldFilms'>
-        <div className="oldFilms_container">
-            <div className="oldFilms_container_top">
-                <h1>OLD FILMS</h1>
-            </div>
-            <div className="oldFilms_container_bottom">
-                <div className="categorySide">
-                    <p>Categories</p>
-                    <div className="types">
-                        <button>ALL</button>
-                        <button>DRAMA</button>
-                        <button>COMEDY</button>
-                        <button>WAR</button>
+import React, { useEffect, useState } from "react";
+import "./OldFilmsComponents.scss";
+import OldFilmsCard from "./OldFilmsCard/OldFilmsCard";
 
-                    </div>
-                </div>
-                <div className="cards">
-                    {
-                        dbData.map((item)=>
-                        
-                        <OldFilmsCard year={item.year} category={item.category} image={item.image} title={item.title} desc={item.desc} duration={item.duration}/>
-                        )
-                    }
-                </div>
-            </div>
+function OldFilmsComponents() {
+  const [dbData, setDbData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [filmsPerPage] = useState(6);
+  const [sortBy, setSortBy] = useState(null)
+  async function fetchData() {
+    const response = await fetch("http://localhost:3003/film");
+    const data = await response.json();
+    setDbData(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const indexOfLastFilm = currentPage * filmsPerPage;
+  const indexOfFirstFilm = indexOfLastFilm - filmsPerPage;
+  const currentFilms = dbData.slice(indexOfFirstFilm, indexOfLastFilm);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <div className="oldFilms">
+      <div className="oldFilms_container">
+        <div className="oldFilms_container_top">
+          <h1>OLD FILMS</h1>
         </div>
+        <div className="oldFilms_container_bottom">
+          <div className="categorySide">
+            <p>CATEGORIES</p>
+            <div className="types">
+              <button>ALL</button>
+              <button onClick={()=>setSortBy({field:"category",asc:true})}>DRAMA</button>
+              <button>COMEDY</button>
+              <button>WAR</button>
+            </div>
+          </div>
+          <div className="cards">
+            {currentFilms
+            
+            .map((item, index) => (
+              <OldFilmsCard
+                key={index}
+                year={item.date}
+                category={item.category}
+                image={item.image}
+                title={item.title}
+                desc={item.desc}
+                duration={item.duration}
+              />
+            ))}
+          </div>
+          <div className="pagination">
+            <div className="container">
+              {Array.from(
+                { length: Math.ceil(dbData.length / filmsPerPage) },
+                (_, i) => (
+                  <button key={i} onClick={() => paginate(i + 1)}>
+                    {i + 1}
+                  </button>
+                )
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default OldFilmsComponents
+export default OldFilmsComponents;
